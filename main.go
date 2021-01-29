cf7c8661dbbd9253f224730f4918f0661ff1baf8
Sajid Ansari <sajidansari33272@gmail.com>
2021-01-29 22:37:56 +0545

    project initialized

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main() {
+
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+
+		// fmt.Println("connected successfull")
+
+		// s.SetContext("")
+		// s.Join("chat")
+		// s.Emit("message", "Welcom to the chat")
+
+		// server.BroadcastToRoom("", "chat", "message", "user with id"+s.ID()+"join the chat")
+
+		// fmt.Println("connected:")
+
+		// return nil
+	})
+
+	server.OnEvent("/", "chat message", func(s socketio.Conn, msg string) {
+		s.SetContext(msg)
+		server.BroadcastToRoom("", "chat", "chat message", msg)
+		fmt.Println("notice:", msg)
+
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+
+		fmt.Println("User with id left the chat", s.ID())
+		fmt.Println("closed", reason)
+
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+}
